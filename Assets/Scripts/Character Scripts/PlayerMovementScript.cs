d0491c42d2aca49df06f8240f6799739d4089b77
Steven404 <50583433+Steven404@users.noreply.github.com>
2021-09-09 11:20:42 +0300

    First day

---
+++ Assets/Scripts/Character Scripts/PlayerMovementScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
